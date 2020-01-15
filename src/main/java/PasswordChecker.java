71f6d90680d8782e70371c3f0aa75ca0e92c0566
MorganNkosi <54417130+MorganNkosi@users.noreply.github.com>
2020-01-15 15:50:14 +0200

    Add files via upload

---
+++ src/main/java/PasswordChecker.java
@@ -0,0 +1,114 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordChecker {
+    private static final Logger logger = LogManager.getLogger(PasswordChecker.class.getName());
+
+    public static void main(String[] args)
+    {
+        String pass = "Helloworld@";
+        logger.info(passwordIsValid(pass));
+    }
+
+    public static boolean hasSpecial(String password)
+    {
+        Pattern sPattern = Pattern.compile("[a-zA-Z0-9]*");
+        Matcher aMatcher = sPattern.matcher(password);
+
+        if (!aMatcher.matches())
+        {
+            return true;
+        }
+        else
+        {
+            logger.error("password should have at least one special character");
+            return false;
+        }
+    }
+
+    public static boolean passwordIsValid(String password)
+    {
+            if (password != " ")
+            {
+                if(password.length() > 7)
+                {
+                    logger.info("length is ok");
+
+                    if(passwordIsOk(password))
+                    {
+                        logger.info("Password meets at least 3 conditions");
+
+                        if (hasSpecial(password))
+                        {
+                            logger.info("password is strong and valid!!");
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    logger.error("password should be longer than 8 characters");
+
+                    return false;
+                }
+            }
+            else
+            {
+                logger.error("Password should exist");
+                return false;
+            }
+    }
+
+    public static boolean passwordIsOk(String password)
+    {
+        boolean hasNum = false, hasCap = false, hasLow = false;
+        char c;
+        int condition = 0;
+        for (int i = 0; i < password.length(); i++)
+        {
+            c = password.charAt(i);
+            if(Character.isDigit(c))
+            {
+                hasNum = true;
+            }
+            if (Character.isUpperCase(c))
+            {
+                hasCap = true;
+            }
+            if (Character.isLowerCase(c))
+            {
+                hasLow = true;
+            }
+            if(hasNum && hasCap && hasLow)
+            {
+                logger.debug("User password is ok");
+                return true;
+            }
+        }
+        if(hasNum == false)
+        {
+            logger.error("password should have at least one digit");
+        }
+        if(hasLow == false)
+        {
+            logger.error("password should have at least one lower case letter");
+        }
+        if(hasCap == false)
+        {
+            logger.error("password should have at least one upper case letter");
+        }
+
+        logger.error("User password is not ok");
+        return false;
+    }
+}
